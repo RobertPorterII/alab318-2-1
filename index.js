5b23f63bebe42b1144c378558625479c010df207
RobertPorterII <au8494@protonmail.com>
2024-10-03 12:03:20 -0400

    Lab start

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const PORT = 3000;
+
+
+// setup template/ engine
+
+
+
+
+
+// middleware setup
+
+
+
+
+// routes
+
+
+
+
+//checking port 
+app.listen(PORT, ()=> console.log(`Server is up at port: ${PORT}`));
